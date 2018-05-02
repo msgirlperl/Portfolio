e2fbeda170c4d2686161df5c738f2926df22e184
msgirlperl <mspeert@gmail.com>
2018-05-02 14:13:28 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function() {
+
+	$('#slides').superslides({
+			animation: 'fade',
+			play: 5000,
+			pagination: false
+		});
+
+	var typed = new Typed(".typed", {
+				strings: ["Software Engineer.", "Web Developer.", "UI/UX Enthusiast."],
+				typeSpeed: 70,
+				loop:true,
+				startDelay: 1000,
+				showCursor: false
+			}
+		);
+
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+	let skillsTopOffset = $(".skillsSection").offset().top;
+	let statsTopOffset = $(".statsSection").offset().top;
+	let countupFinished = false;
+
+	$(window).scroll(() => {
+
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+	            easing: 'easeInOut',
+	            barColor: '#fff',
+	            trackColor: false,
+	            scaleColor: false,
+	            lineWidth: 4,
+	            size: 152,
+	            onStep: function(from, to, percent) {
+	            	$(this.el).find('.percent').text(Math.round(percent));
+    			}
+   			 });
+
+		}
+
+
+		if (!countupFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+
+			$(".counter").each(function () {
+				let element = $(this);
+				let endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			});
+
+			countupFinished = true;
+		}
+	});
+	
+});
