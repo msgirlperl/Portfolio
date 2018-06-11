c55fbf3841512c10c4bc050f845d71116d32f043
msgirlperl <mspeert@gmail.com>
2018-06-11 13:05:58 -0400

    finished items and filter, copyright, layouts

--- js/script.js
+++ js/script.js
@@ -83,4 +83,22 @@ $(document).ready(function() {
 		}
 	});
 
+	$("#filters a").click(function() {
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		const selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+					duration: 1500,
+					easing: 'linear',
+					queue: false
+			}
+		});
+
+		return false
+	});
+
 });
