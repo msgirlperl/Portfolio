e74f0f5fa9898785e84f5730da53e50518284475
msgirlperl <mspeert@gmail.com>
2018-06-20 15:31:22 -0400

    swapped out for Reece's script.jss and css to debug page issues

--- js/script.js
+++ js/script.js
@@ -1,133 +1,129 @@
-$(window).on("load", function() {
-	$(".loader .inner").fadeOut(750, function() {
-		$(".loader").fadeOut(1000);
-	});
+$(window).on('load', function() {
+  $('.loader .inner').fadeOut(500, function() {
+    $('.loader').fadeOut(750);
+  });
 });
 
 $(document).ready(function() {
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false
+  });
 
-	$('#slides').superslides({
-			animation: 'fade',
-			play: 5000,
-			pagination: false
-		});
+  var typed = new Typed('.typed', {
+    strings: ['Software Engineer.', 'Web Developer.', 'Student.'],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 
-	var typed = new Typed(".typed", {
-				strings: ["Software Engineer.", "Web Developer.", "UI/UX Enthusiast."],
-				typeSpeed: 70,
-				loop:true,
-				startDelay: 1000,
-				showCursor: false
-			}
-		);
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      938: {
+        items: 4
+      }
+    }
+  });
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        }
-	    }
-	});
+  var skillsTopOffset = $('.skillsSection').offset().top;
+  var statsTopOffset = $('.statsSection').offset().top;
+  var countUpFinished = false;
+  $(window).scroll(function() {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find('.percent')
+            .text(Math.round(percent));
+        }
+      });
+    }
 
-	let skillsTopOffset = $(".skillsSection").offset().top;
-	let statsTopOffset = $(".statsSection").offset().top;
-	let countupFinished = false;
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $('.counter').each(function() {
+        var element = $(this);
+        var endVal = parseInt(element.text());
 
-	$(window).scroll(() => {
+        element.countup(endVal);
+      });
 
-		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      countUpFinished = true;
+    }
+  });
 
-			$('.chart').easyPieChart({
-	            easing: 'easeInOut',
-	            barColor: '#fff',
-	            trackColor: false,
-	            scaleColor: false,
-	            lineWidth: 4,
-	            size: 152,
-	            onStep: function(from, to, percent) {
-	            	$(this.el).find('.percent').text(Math.round(percent));
-    			}
-   			 });
-		}
+  $('[data-fancybox]').fancybox();
 
-		if (!countupFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+  $('.items').isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
 
-			$(".counter").each(function () {
-				let element = $(this);
-				let endVal = parseInt(element.text());
+  $('#filters a').click(function() {
+    $('#filters .current').removeClass('current');
+    $(this).addClass('current');
 
-				element.countup(endVal);
-			});
+    var selector = $(this).attr('data-filter');
 
-			countupFinished = true;
-		}
-	});
+    $('.items').isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+      }
+    });
 
-	$("[data-fancybox]").fancybox();
+    return false;
+  });
 
-	$(".items").isotope({
-		filter: '*',
-		animationOptions: {
-				duration: 1500,
-				easing: 'linear',
-				queue: false
-		}
-	});
+  $('#navigation li a').click(function(e) {
+    e.preventDefault();
 
-	$("#filters a").click(function() {
-		$("#filters .current").removeClass("current");
-		$(this).addClass("current");
+    var targetElement = $(this).attr('href');
+    var targetPosition = $(targetElement).offset().top;
+    $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
+  });
 
-		const selector = $(this).attr("data-filter");
+  const nav = $('#navigation');
+  const navTop = nav.offset().top;
 
-		$(".items").isotope({
-			filter: selector,
-			animationOptions: {
-					duration: 1500,
-					easing: 'linear',
-					queue: false
-			}
-		});
+  $(window).on('scroll', stickyNavigation);
 
-		return false
-	});
+  function stickyNavigation() {
+    var body = $('body');
 
-	$("#navigation li a").click(function(e) {
-			e.preventDefault();
-
-			let targetElement = $(this).attr("href");
-			let targetPosition = $(targetElement).offset().top;
-			$("html, body").animate({ scrollTop: targetPosition  - 50 }, "slow");
-
-
-	});
-
-	const stickyNavigation = () => {
-
-		let body = $("body");
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
-		} else {
-			body.css("padding-top", 0);
-			body.removeClass("fixedNav");
-		}
-	};
-
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-
+    if ($(window).scrollTop() >= navTop) {
+      body.css('padding-top', nav.outerHeight() + 'px');
+      body.addClass('fixedNav');
+    } else {
+      body.css('padding-top', 0);
+      body.removeClass('fixedNav');
+    }
+  }
 });
